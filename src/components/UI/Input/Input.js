f57c2a5acee2b8c2e92eae1ad1914103279f50a6
Bill Buchanan <bbuchanan@gmail.com>
2017-12-13 16:28:00 -0500

    completed validation by checking if the form is valid and disabling/enabling the submit button accordingly

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,23 +3,28 @@ import classes from './Input.css'
 
 const input = (props) => {
   let inputElement = null
+  const inputClasses = [classes.InputElement]
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid)
+  }
   switch (props.elementType) {
     case ('input'):
       inputElement = <input
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig} value={props.value}
         onChange={props.changed} />
       break
     case ('textarea'):
       inputElement = <textarea
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig} value={props.value}
         onChange={props.changed} />
       break
 
     case ('select'):
       inputElement = (<select
-          className={classes.InputElement}
+          className={inputClasses.join(' ')}
           onChange={props.changed}
           value={props.value}>
           {props.elementConfig.options.map(option =>
@@ -33,13 +38,13 @@ const input = (props) => {
 
       default:
       inputElement = <input
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig} value={props.value}
         onChange={props.changed} />
   }
     
   return (
-    <div className={classes.Input}>
+    <div className={inputClasses.join(' ')}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
     </div>
