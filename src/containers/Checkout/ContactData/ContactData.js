7fdb5b48ea65857d65610f59c7378ceff82cc5d6
Bill Buchanan <bbuchanan@gmail.com>
2017-12-13 15:47:50 -0500

    submitting the form with the dynamic contact data fields in the state object

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -61,15 +61,19 @@ class ContactData extends Component {
     },
     loading: false
   }
-
-  componentDidMount() {
-  }
+  
   orderHandler = (event) => {
     event.preventDefault()
     this.setState({ loading: true })
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
+
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -106,7 +110,7 @@ class ContactData extends Component {
       })
     }
     let form = (
-    <form>
+    <form onSubmit={this.orderHandler}>
       {formElementsArray.map(formElement => (
         <Input
           changed={(event) => this.inputChangedHandler(event, formElement.id)}
@@ -115,7 +119,7 @@ class ContactData extends Component {
           elementConfig={formElement.config.elementConfig}
           value={formElement.config.value} />
       ))}
-      <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
+      <Button btnType="Success">ORDER</Button>
     </form>
     )
     if (this.state.loading) {
