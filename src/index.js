54ab09228dffe5e51e2d1150a9a9f33ae2b08f2e
Bill Buchanan <bbuchanan@gmail.com>
2018-01-04 17:18:11 -0500

    finished moving all global state to redux, specifically the contact data, the loading state, initial state. Made use of the 2 reducers in the index.js file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducers/burgerBuilder'
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import orderReducer from './store/reducers/order'
 
 // const logger = store => {
 //   return next => {
@@ -22,9 +23,13 @@ import reducer from './store/reducers/burgerBuilder'
 // }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+})
 //const store = createStore(reducer, applyMiddleware(logger))
 //const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
   applyMiddleware(thunk)))
 
 const app = (
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -17,13 +17,21 @@ export const purchaseBurgerFail = (error) => {
   }
 }
 
+export const purchaseBurgerStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  }
+}
+
 // next, add matching async action
-export const purchaseBurgerState = (orderData) => {
+
+export const purchaseBurger = (orderData) => {
   return dispatch => {
+    dispatch(purchaseBurgerStart())
     axios.post('/orders.json', orderData)
       .then(response => {
         console.log(response.data)
-        dispatch(purchaseBurgerSuccess(response.data, orderData))
+        dispatch(purchaseBurgerSuccess(response.data.name, orderData))
       })
       .catch(error => {
         dispatch(purchaseBurgerFail(error))
