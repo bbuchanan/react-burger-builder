40f28f3bc778545ccfa9f74d35ffb032a2308344
Bill Buchanan <bbuchanan@gmail.com>
2018-01-05 16:25:51 -0500

    resetting the state and redirecting upon completion of an order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -16,9 +16,12 @@ class Checkout extends Component {
 
   render () {
     let summary = <Redirect to="/" />
+    
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null
       summary = (
         <div>
+          {purchasedRedirect}
         <CheckoutSummary ingredients={this.props.ings}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler} />
@@ -33,7 +36,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   }
 }
 
