792ffc7049b541c24f7d92cfc6e4f03c42756b93
Bill Buchanan <bbuchanan@gmail.com>
2018-01-09 13:35:46 -0500

    storing auth data (token) in the auth redux store

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import orderReducer from './store/reducers/order'
+import authReducer from './store/reducers/auth'
 
 // const logger = store => {
 //   return next => {
@@ -25,7 +26,8 @@ import orderReducer from './store/reducers/order'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 })
 //const store = createStore(reducer, applyMiddleware(logger))
 //const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
