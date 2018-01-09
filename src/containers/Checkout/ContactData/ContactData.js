debb61b89249ab8325bc89fac050c5308a72cc97
Bill Buchanan <bbuchanan@gmail.com>
2018-01-09 16:39:38 -0500

    refactoring the updateObject function to be shared. Changed auth and contact data forms to use the updateObject like the store objects do

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input'
 import classes from './ContactData.css'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { updateObject } from '../../../shared/utility'
 
 class ContactData extends Component {
   state = {
@@ -128,17 +129,17 @@ class ContactData extends Component {
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm
-    }
+    //const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
+    const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+      value: event.target.value,
+      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      touched: true
+    })
 
-    const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement
+    })
 
-    updatedFormElement.value = event.target.value
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-    updatedFormElement.touched = true
-    updatedOrderForm[inputIdentifier] = updatedFormElement
-  
     let formIsValid = true
     for (let inputIdentifiers in updatedOrderForm) {
       formIsValid = updatedOrderForm[inputIdentifiers].valid && formIsValid
