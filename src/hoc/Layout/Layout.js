0457b269fe9535edd14e93b171a5edbb84256bda
Bill Buchanan <bbuchanan@gmail.com>
2018-01-09 14:18:39 -0500

    propagating the authenticated status to layout components so that they can reflect this in the user interface

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import Aux from '../Aux/Aux'
 import classes from './Layout.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
@@ -22,8 +23,8 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}></Toolbar>
-        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <Toolbar isAuth={this.props.isAuthenticated} drawerToggleClicked={this.sideDrawerToggleHandler}></Toolbar>
+        <SideDrawer isAuth={this.props.isAuth} open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
           {this.props.children}
         </main>
@@ -31,6 +32,11 @@ class Layout extends Component {
     )
   }
 }
-  
 
-export default Layout
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token != null
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
